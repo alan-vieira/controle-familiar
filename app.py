9fb0023d7caf01c7fa726d2cdaf5c43c2a465669
alan-vieira <alansilvavieira@gmail.com>
2025-11-20 21:25:35 -0300

    fix: correção app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def create_app():
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+        expose_headers=["Set-Cookie"]
     )
 
     # Configuração do Flask-Login

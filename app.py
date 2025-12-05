d3491c639e163d7043f16ee941bd295d665c38f2
alan-vieira <alansilvavieira@gmail.com>
2025-12-05 00:13:15 -0300

    fix: reparando o app.py

--- app.py
+++ app.py
@@ -44,13 +44,8 @@ def create_app():
     return app
 
 
-# Ponto de entrada para o Render
-def application(environ, start_response):
-    app = create_app()
-    return app(environ, start_response)
-
+application = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
